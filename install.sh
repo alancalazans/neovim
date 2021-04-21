f6460f870e3d566136d025f1cfec0096e91b2e98
alancalazans <alan2calazans@gmail.com>
2021-04-21 15:20:05 -0300

    2021.04.21 15:20

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 chmod +x nvim.appimage
 ./nvim.appimage --appimage-extract
 sudo mv squashfs-root/ /opt/nvim
-#sudo chown $USER -R /opt/nvim
 sudo ln -sf /opt/nvim/usr/bin/nvim /usr/bin/nvim
 unzip _nvim.zip -d $HOME/.config/
+unzip _nvim.zip -d /root/.config/
 clear
 echo '[Desktop Entry]
 Version=0.4.4
@@ -20,7 +20,4 @@ Keywords=Text;editor;
 Categories=Utility;TextEditor;Development;
 StartupNotify=false
 MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;' | sudo tee /usr/share/applications/nvim.desktop
-sudo chmod 755 /usr/share/applications/nvim.desktop	
-#sudo chown $USER /usr/share/applications/nvim.desktop
-sudo cp -r $HOME/.config/nvim /root/.config/
-
+sudo chmod 755 /usr/share/applications/nvim.desktop
