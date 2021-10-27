bb7ec86a9ef6bdd0328ed7d71615976f1099efaf
alancalazans <alan2calazans@gmail.com>
2021-10-27 07:18:28 -0300

    2021.10.27 07:18

--- extract.sh
+++ extract.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 tar -Jxvf _nvim.tar.xz -C $HOME/.config/
+cp snippets/* $HOME/.config/nvim/snippets/
 cp init.vim $HOME/.config/nvim/
 sudo tar -Jxvf _nvim.tar.xz -C /root/.config/
+sudo cp snippets/* /root/.config/nvim/snippets/
 sudo cp init.vim /root/.config/nvim/
