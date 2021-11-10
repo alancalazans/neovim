014854592210352db6498623049f45b2a3825317
alancalazans <alan2calazans@gmail.com>
2021-11-10 11:05:53 -0300

    2021.11.10 11:05

--- extract.sh
+++ extract.sh
@@ -2,6 +2,11 @@
 tar -Jxvf _nvim.tar.xz -C $HOME/.config/
 cp snippets/* $HOME/.config/nvim/snippets/
 cp init.vim $HOME/.config/nvim/
+mkdir $HOME/.local/share/fonts
+cp Droid*.otf $HOME/.local/share/fonts/
+#Para root
 sudo tar -Jxvf _nvim.tar.xz -C /root/.config/
 sudo cp snippets/* /root/.config/nvim/snippets/
 sudo cp init.vim /root/.config/nvim/
+sudo mkdir -p /root/.local/share/fonts
+sudo cp Droid*.otf /root/.local/share/fonts/
