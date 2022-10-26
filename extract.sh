70b59d319ac5ca294967607d6f980d2f3d8b9b49
alancalazans <alan2calazans@gmail.com>
2022-10-26 09:23:51 -0300

    2022.10.26 09:23

--- extract.sh
+++ extract.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-tar -Jxvf _nvim.tar.xz -C $HOME/.config/
+tar -Jxf _nvim.tar.xz -C $HOME/.config/
 cp snippets/* $HOME/.config/nvim/snippets/
 cp init.vim $HOME/.config/nvim/
+cp ginit.vim $HOME/.config/nvim/
 mkdir $HOME/.local/share/fonts
 cp Droid*.otf $HOME/.local/share/fonts/
+unzip NeoVim_fonts/FiraMono.zip -d $HOME/.local/share/fonts/
 #Para root
-sudo tar -Jxvf _nvim.tar.xz -C /root/.config/
+sudo tar -Jxf _nvim.tar.xz -C /root/.config/
 sudo cp snippets/* /root/.config/nvim/snippets/
 sudo cp init.vim /root/.config/nvim/
+sudo cp ginit.vim /root/.config/nvim/
 sudo mkdir -p /root/.local/share/fonts
 sudo cp Droid*.otf /root/.local/share/fonts/
+sudo unzip NeoVim_fonts/FiraMono.zip -d /root/.local/share/fonts/
