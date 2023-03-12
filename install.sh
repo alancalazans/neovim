62003cf5b21d358ea6eb0089d0648f41ee849e0f
alancalazans <alan2calazans@gmail.com>
2023-03-12 09:32:57 -0300

    2023.03.12 09:32

--- install.sh
+++ install.sh
@@ -20,5 +20,40 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvi
 #--------------------
 # Após a instalação do `packer.nvim`
 # abrir o `nvim` e no modo normal
-# executar o comando `PackerInstall`.
+# executar o comando `:PackerInstall`.
 #--------------------
+#--------------------
+# ALE
+# Github: https://github.com/dense-analysis/ale
+# Instalando:
+#--------------------
+mkdir -p ~/.local/share/nvim/site/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.local/share/nvim/site/pack/git-plugins/start/ale
+#--------------------
+# ESLint
+# https://eslint.org/
+# Instalando:
+#--------------------
+npm install eslint -g
+#--------------------
+# Criando arquivo de configuração
+# `.eslintrc.js` na pasta de usuário:
+#--------------------
+echo -e 'module.exports = {
+    "env": {
+			"browser": true,
+			"es2021": true
+    },
+    "overrides": [
+    ],
+    extendes: [
+    	"standard",
+    	"eslint:recommended",
+    	"prettier"
+    ],
+    "parserOptions": {
+			"ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}' > ~/.eslintrc.js
