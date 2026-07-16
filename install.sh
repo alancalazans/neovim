4081c703526799c134039a4ad61f0db5dd1fb7e1
alancalazans <alan2calazans@gmail.com>
2026-07-16 14:24:57 -0300

    2026.07.16 14:24

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 curl -LO $(curl -s https://api.github.com/repos/neovim/neovim/releases/latest | grep -o 'https://github.com/neovim/neovim/releases/download/[^"]*nvim-linux-x86_64.tar.gz')
 sudo tar -xf nvim-linux-x86_64.tar.gz -C /opt/
 sudo mv /opt/nvim-linux-x86_64 /opt/nvim
+sudo cp plug.vim /opt/nvim/share/nvim/runtime/autoload/
 sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/nvim
 mkdir -p ~/.config/nvim
 cp init.vim ~/.config/nvim/
